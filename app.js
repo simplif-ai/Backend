39898ae2af3766611c7e05a83763a7a057bfff67
sblatz <sblatz@purdue.edu>
2017-10-01 17:15:35 -0400

    Google login endpoint created

--- app.js
+++ app.js
@@ -72,6 +72,57 @@ app.post('/savetodb', function(req, res) {
     //sends to db 
 })
 
+//Google authentication setup
+var GoogleAuth; // Google Auth object.
+function initClient() {
+  gapi.client.init({
+      'apiKey': 'YOUR_API_KEY',
+      'clientId': 'YOUR_CLIENT_ID',
+      'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
+  }).then(function () {
+      GoogleAuth = gapi.auth2.getAuthInstance();
+
+      // Listen for sign-in state changes.
+      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+
+      // Handle initial sign-in state. (Determine if user is already signed in.)
+      var user = GoogleAuth.currentUser.get();
+      setSigninStatus();
+
+    });
+}
+
+function setSigninStatus(isSignedIn) {
+    var user = GoogleAuth.currentUser.get();
+    var isAuthorized = user.hasGrantedScopes(SCOPE);
+    if (isAuthorized) {
+      //set something about being authorized
+    } else {
+      //let the user know they're not authorized
+    }
+  }
+
+//login endpoint
+//allows the user to login with google authentication
+app.post('/login', function(req, res)) {
+	if (GoogleAuth.isSignedIn.get()) {
+		//user is already signed in!
+    } else {
+      // User is not signed in. Start Google auth flow.
+      GoogleAuth.signIn();
+    }
+}
+
+
+
+//create account endpoint
+//lets the user create an account without google authentication
+app.post('/createAccount', function(req, res)) {
+
+
+}
+
 app.listen('8000');
 console.log('Listening on port ' + 8000 + '...');
 
