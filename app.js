599c9a2cf11f4f316939e48e0b8b6e79ac815f7b
larafa <larafa@purdu.edu>
2017-10-05 17:54:36 -0400

    working on testing db and implementing mail function

--- app.js
+++ app.js
@@ -13,31 +13,39 @@ const bodyparser = require('body-parser');
 const path = require('path');
 //const expressValidator = require('express-validator');
 const request = require('request');
+const mysql = require('mysql');
+const nodemailer = require ('nodemailer');
 
-//add dependencies for database
 //setup database
+var connection = mysql.createConnection({
+    host: 'simplifai.caijj6vztwxw.us-east-2.rds.amazonaws.com',
+    user: 'simplifai',
+    password: '7uRr)MkBu[pabQv3',
+    port: '3306'
+});
+
 
 //parse application/JSON
 app.use(bodyparser.json());
 
-//User endpoint
-//Forget passowrd mailto endpoint
 
 //this is a mock api to test the fuctionality of the
 //middleware function
-app.get('/mocktext', function(req, res) {
+app.get('/mocktext', function (req, res) {
     var mock = "Hi this is Lena's mock text";
     var json = {
         'text': mock
     }
-        var options = {
+    var options = {
         url: 'http://localhost:8000/sumarizertext',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(json) 
+        body: JSON.stringify(json)
     }
-    request.post(options, function(error, response, body){
+   // res.send(options.body);
+    request.post(options, function (error, response, body) {
+        //console.log(body);
         res.send(JSON.parse(body));
     })
 })
@@ -45,11 +53,12 @@ app.get('/mocktext', function(req, res) {
 //Text endpoint; text sumbitted by user is handled here
 //It is then sent to the summarizer api and the data received
 //is sent back to the user
-app.post('/sumarizertext', function(req, res){
+app.post('/sumarizertext', function (req, res) {
     //url subject to change once api is created
-    var summarizerApi = "https://ir.thirty2k.com/summarizeTEST";
+    var summarizerApi = "https://ir.thirty2k.com/summarize";
     var options = {
         headers: {
+            'Accept' : 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(req.body)
@@ -57,19 +66,95 @@ app.post('/sumarizertext', function(req, res){
 
     //send the text received from user to api of summarizer
     //get for testing reasons, use post when using summarizer api url
-    request.get(summarizerApi, options, function(error, response, body) {
+    request.post(summarizerApi, options, function (error, response, body) {
         //recives data from summarizerAPI
         //sends it back to the summarizertext endpoint which would be the
         //body response to any request that posts a request to it
         //uses json to send a stringfied json object of the non-object data from api
-        res.json(body);
-    })   
+        console.log('statusCode', response.statusCode);
+        if (!error && response.statusCode === 200) {
+            res.send(body);
+          } else {
+            res.send({ success: false, error: error });
+          }
+    })
 })
 
 //another request to get the saved version from the user of the summarizer text
 //and send it to db
-app.post('/savetodb', function(req, res) {
-    //sends to db 
+app.post('/savetodb', function (req, res) {
+    connection.connect(function (err) {
+        if (err) {
+            console.error('Database connection failed: ' + err.stack);
+            return;
+        }
+        console.log('Connected to database.');
+        var sql = "ALTER TABLE User ADD COLUMN summary STRING AUTO_INCREMENT PRIMARY KEY";
+        connection.query(sql, function(err, result){
+            if(err) {
+                console.error('Error with adding summary column');
+            }
+            console.log('Table Altered');
+
+            sql = "INSERT INTO Users (summary) VALUES ("+req.body+")";
+        })
+        connection.query(sql, function(err, result){
+            if(err) {
+                console.error('Error with instering summary in table');
+            }
+            console.log('summary record inserted');
+        })
+    })
+    connection.end();
+})
+
+//request to receive forget password post and send an email to user
+app.post('/mailto', function (req, res) {
+    //get email of user from db
+    //receive email and link to reset?
+    var email;
+    // connection.connect(function (err) {
+    //     if (err) {
+    //         console.error('Database connection failed: ' + err.stack);
+    //         return;
+    //     }
+    //     console.log('Connected to database.');
+    //         sql = "SELECT email FROM Users where idUser = ";
+    //     connection.query(sql, function(err, result, fields){
+    //         if(err) {
+    //             console.error('Error with retrieving email from table');
+    //         }
+    //         email = result;
+    //         console.log('email retreived');
+    //     })
+    // })
+    // connection.end();
+
+     //send email
+    //text has the paramter url of connecting to the page for resetting password
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'simplif.ai@gmail.com',
+          pass: 'simplif.ai2017'
+        }
+      });
+    var mailOptions = {
+        from: 'simplif.ai@gmail.com',
+        to: email,
+        subject: 'Reset password to Simplif.ai', 
+        text: req.param.url
+    }
+
+    transporter.sendMail(mailOptions, function(error, infor){
+        if(error) {
+            console.log('error sending email for resetting password');
+        }
+        else {
+            console.log('Email sent: ' + req.param.url);
+        }
+    })
+    
 })
 
 //Google authentication setup
