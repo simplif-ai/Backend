e284f046bef7aa0643031a09882ce0d76d21e6ec
sblatz <sblatz@purdue.edu>
2017-11-28 13:05:30 -0500

    All test cases working for article parsing

--- dbFunc/parseArticles.js
+++ dbFunc/parseArticles.js
@@ -25,10 +25,17 @@ module.exports = function (app) {
 
       if (!URL) {
          res.status(500).send({success: false, error: "No URL found."});
+         return;
       }
       console.log('URL: ' + URL);
 
       request(URL, function (error, response, body) {
+
+        if (error) {
+          res.status(500).send({success: false, error: error});
+          return;
+        }
+
         let text = extractor(body).text;
         console.log('text: ' + text);
 
@@ -47,26 +54,14 @@ module.exports = function (app) {
             'http://simplif-ai-backend.us-east-2.elasticbeanstalk.com/summarizertext',
              options ,
             function (error, response, body) {
-              console.log(body);
                 if (!error && response.statusCode == 200) {
+                    console.log('sending body');
                     res.status(200).send(response.body);
                 } else {
-                    rest.status(500).send({success: false, error: error});
+                    res.status(500).send({success: false, error: error});
                 }
             }
         );
       });
     });
-
-    /*
-
-      connection.query("UPDATE users SET darkMode = ? WHERE idUser = ?", [darkMode, userID], function (err, result) {
-        if (err) {
-          res.status(500).send({success: false, error: err});
-        } else {
-           res.status(200).send({error: null, success: true});
-        }
-      });
-    });
-    */
 }
